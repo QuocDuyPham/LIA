e890d0cceddfa44db33dea0a3e1446565d71f67b
QuocDuyPham <126590789+QuocDuyPham@users.noreply.github.com>
2023-05-23 19:54:46 -0400

    Update ManualControl.ino

--- ManualControl.ino
+++ ManualControl.ino
@@ -1,50 +1,47 @@
-int sensor = A1;
-int intputPin= A3;
-int actuator = 7;
-int PV = 0;
-int inputValue = 0;
-int CO = 0;
-int PushButton = 13;
+// Manual Control (/*)
+// The manual control system is implemented in this code using the analog sensor (LDR) and an actuator (LED). 
+// A potentiometer (variable resistor) is used to change the input value in order to change the output. 
+
+// Both the Serial Monitor and an LCD screen show the Input Value, Process Variable (PV), and Control Output (CO) readings.
+// */
+int sensor = A5;// Set the pin for the Sensor
+int intputPin= A3; // Set the pin for the potentiometer which will be the SP
+int actuator = 6; //  digital pin for the LED
+
+int PV = 0; // The process variable which represents LDR value
+int inputValue = 0; // Potentiometer value
+int CO = 0; // Control output value
 #include <LiquidCrystal.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-LiquidCrystal lcd(2, 3, 9, 10, 11, 12); 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(actuator, OUTPUT);
-  pinMode(PushButton  , INPUT_PULLUP);
-  Serial.begin(9600);
-   lcd.begin(16, 2);
+  pinMode(actuator, OUTPUT); // Make the actuator pin as an OUTPUT
+  Serial.begin(9600);  // Initalize Serial Monitor with baud rate of 9600
+   lcd.begin(16, 2); // Set up LCD's number of colums and rows
 
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  int BinValue = digitalRead(PushButton);
-  Serial.print( "PB = ");
-  Serial.print(BinValue);
   PV = analogRead(sensor);
+  // Read the input value ranges from 0 to 1023.
   inputValue= analogRead(intputPin);
+    // Map the input value to the control output range of 0 to 255
   CO = map(inputValue, 0, 1023, 0, 255); 
+  // Writing the  (CO) to the actuator (LED)
   analogWrite(actuator,CO);
 
-  Serial.print(" \t input = ");
-  Serial.print(inputValue);
+  // Serial.print(" \t input = ");
+  // Serial.print(inputValue);
   Serial.print(" \t CO = ");
   Serial.print(CO);
   Serial.print(" \t PV = ");
   Serial.print(PV);
   Serial.println();
-if (BinValue == 1){
-lcd.setCursor(0,0); // set the postion on the lcd start from colunm 0 of the first row
-lcd.print("CO = "); // lcd print the line: volatge = 
-lcd.println(CO); // lcd print the voltage value
-lcd.setCursor (0,1);
-lcd.print("Input = "); // lcd print the line: volatge = 
-lcd.println(inputValue); // lcd print the voltage value
-}
-else {
+
 lcd.setCursor(0,0); // set the postion on the lcd start from colunm 0 of the first row
 lcd.print("CO = "); // lcd print the line: volatge = 
 lcd.println(CO);
@@ -53,4 +50,3 @@ lcd.print (" PV = "); // lcd print the line: Bitvalue
 lcd.println(PV); // lcd print the Bit value
 }
 
-}
